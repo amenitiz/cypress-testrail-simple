ccce57cf8fe03c01e7f64c94d914a64190bb68d0
abramovaav <abramovaav8@gmail.com>
2022-02-17 18:14:59 +0400

    Add add plan

--- bin/testrail-add-plan.js
+++ bin/testrail-add-plan.js
@@ -11,25 +11,18 @@ const { getTestSuite } = require('../src/testrail-api')
 
 const args = arg(
   {
-    '--name': String,
-    '--nameRun': String,
-    '--suite': String,
+    '--planName': String,
     // aliases
-    '-n': '--name',
-    '-st': '--suite',
-    '-nr': '--nameRun',
+    '-n': '--planName',
   },
   { permissive: true },
 )
 // optional arguments
-const name = args['--name'] || args._[0]
-const nameRun = args['--nameRun'] || args._[1]
-debug('args: %o', args)
-debug('plan name: %s', name)
-debug('run name: %s', nameRun)
+const planName = args['--planName'] || args._[0]
+debug('plan name: %s', planName)
 
 async function addTestPlan({
-  testRailInfo, name, nameRun
+  testRailInfo, planName
 }) {
   console.error(
     'creating new TestRail plan for project %s',
@@ -41,29 +34,9 @@ async function addTestPlan({
   const authorization = getAuthorization(testRailInfo)
 
   const json = {
-    name,
-    entries: [
-      {
-        suite_id: testRailInfo.suiteId,
-        name: nameRun,
-      },
-    ],
+    planName,
   }
-
   debug('add plan params %o', json)
-
-  let suiteId = testRailInfo.suiteId
-  if (suiteId) {
-    // let the user pass the suite ID like the TestRail shows it "S..."
-    // or just the number
-    if (suiteId.startsWith('S')) {
-      suiteId = suiteId.substring(1)
-    }
-    json.suite_id = Number(suiteId)
-    debug('suite id %d', json.suite_id)
-    // simply print all test cases
-    await getTestSuite(suiteId, testRailInfo)
-  }
   // @ts-ignore
   return got(addTestPlanUrl, {
     method: 'POST',
@@ -76,7 +49,7 @@ async function addTestPlan({
     .json()
     .then(
       (json) => {
-        debug('response from the add_run')
+        debug('response from the add_plan')
         debug('%o', json)
         console.log(json.id)
       },
@@ -84,9 +57,6 @@ async function addTestPlan({
         console.error('Could not create a new TestRail run')
         console.error('Response: %s', error.name)
         console.error('Please check your TestRail configuration')
-        if (json.case_ids) {
-          console.error('and the case IDs: %s', json.case_ids)
-        }
         process.exit(1)
       },
     )
@@ -94,4 +64,4 @@ async function addTestPlan({
 const testRailInfo = getTestRailConfig()
   // start a new test run for all test cases
   // @ts-ignore
-addTestPlan({ testRailInfo, name, nameRun })
+addTestPlan({ testRailInfo, planName })
